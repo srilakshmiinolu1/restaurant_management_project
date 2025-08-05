ff6637c6f432884c78ca251b6e592e64445e5868
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-08-05 22:35:37 +0530

    21065

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,9 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import User
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=model.CASCADE)
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    def __str__(self):
-        return self.user.username
+
