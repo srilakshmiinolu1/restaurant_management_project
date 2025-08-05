79c0292b4236cbeeba7d1118d97b7e943211eb50
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-08-05 22:43:38 +0530

    21065

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,1 +1,16 @@
-
+from django.shortcuts import render
+import request
+def menu_view(request):
+    api_url = 'http://localhost:8000/api/menu/'
+    menu_data = []
+    try:
+        response = request.get(api_url)
+        response.raise_for_status()
+        menu_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching menu data: {e}")
+    content = {
+        'menu_items' : menu_data
+    }
+    return render(request, 'menu.html', context)
+            
