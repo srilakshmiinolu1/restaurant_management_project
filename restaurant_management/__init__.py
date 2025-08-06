4a6d514be35b9502ddf09e5691f062e3f7b34597
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-08-06 21:26:22 +0530

    21069

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,6 +1,10 @@
-from rest_framework import serializers
-class MenuItem(serializers.serializer):
-    name = serializers.CharField(max_length=100)
-    description = serializers.CharField()
-    price = serializers.DecimalField(max_degits=5, decimal_places=2)
+from rest_framework import models
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_degits=6, decimal_places=2)
+    def __str__(self):
+        return self.name
+
+
     
