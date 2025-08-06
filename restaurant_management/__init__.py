92bc4db141933b7c667de062a2b020d748497d3a
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-08-06 21:32:03 +0530

    21069

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -2,7 +2,7 @@ from rest_framework import models
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_degits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     def __str__(self):
         return self.name
 
