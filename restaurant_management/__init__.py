c1f7202f549ed8bbdc442f6462b3d9a5aea8f890
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-16 22:53:06 +0530

    44735

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,15 +1,11 @@
-from django.db import models
-from products.models import Product
-class OrderStatus(mmodel.Model):
-    name = models.CharField(max_length=50, unique=True)
-    def __str__(self):
-        return self.name
-class Order(models.Model):
-    customer_name = models.CharField(max_length=100)        
-    order_date = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=10,decimal_places=2)
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, blank=True)
-    products = models.ManyToManyField(Product, related_name="orders")
-    coupon_code = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    def __str__(self):
-        return f"Order #{self.id} - {self.customer_name}"  
+from rest_framework.generics import ListAPIView
+from rest_framework import serializers
+from .models import MenuCategory
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = ['name']
+class MenuCategoryListView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
+
