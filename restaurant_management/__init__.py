84299c17f9db0732a534a27e38d6d0c169f4580a
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-16 22:26:46 +0530

    44736

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,10 +1,15 @@
-from django.apps import AppConfig
-class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orders'
-    def ready(self):
-        from .models import OrderStatus
-        default_statuses = ["pending", "processing", "Completed", "Cancelled"]
-        for status in default_statuses:
-            OrderStatus.objects.get_or_create(name=status)
-            
+from django.db import models
+from products.models import Product
+class OrderStatus(mmodel.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)        
+    order_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, blank=True)
+    products = models.ManyToManyField(Product, related_name="orders")
+    coupon_code = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"  
