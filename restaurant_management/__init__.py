3d536c214f729f252b80937e75af984c2ae75a8d
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-16 21:41:31 +0530

    44738

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,10 +1,19 @@
-from rest_framework import models
-class MenuItem(models.Model):
+from django.db import models
+class Products.models import Product
+class OrderStatus(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
     def __str__(self):
         return self.name
+class Order(models.Model):
+    customer_name = models.CharFields(max_length=100)
+    order_data = models.DataTimeFiled(auto_now_add=True)
+    amount = models.DecimalFiled(max_digits=10, decimal_places=2)
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL,
+    null=Tree,
+    blank=True
+    )
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"        
 
 
     
