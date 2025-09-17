d695977e5a09932c5e46b0d503fc2e3b6e6e1c0f
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-17 23:02:01 +0530

    47214

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_names = "orders")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="orders")
     date = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
-        class OrderItem(models.Model):
-            order = models.ForeignKey(order, on_delete=models.CASCADE, related_names = "items")
-            product_name = models.CharField(max_length=100)
-            quantity = models.PositiveIntegerField()
-            price = models.DecimalField(max_digits=10, decimal_places=2)
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    product_name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     def __str__(self):
         return f"{self.product_name} (x{self.quantity})"
