7546c2acef140ef804f19ae445a2acafcbe1aef7
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-17 23:24:17 +0530

    47213

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,15 +1,9 @@
-from django.db import models
-from django.contrib.auth.models import User
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="orders")
-    date = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    def __str__(self):
-        return f"Order {self.id} by {self.user.username}"
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
-    product_name = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-    def __str__(self):
-        return f"{self.product_name} (x{self.quantity})"
+class MenuItemsByCategoryView(APIView):
+    def get_queryset(slef, request):
+        category = request.query_params.get('category',None)
+        if category:
+            items = MenuItem.objects.filter(category__name__iexact=category)
+            else:
+                items = MenuItem.objects.all()
+                serializer = MenuItemSerializer(items, many=True)
+                return Response(serializer.data)   
