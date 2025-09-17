ac51b1842f5774dd34aacecc8b70be3b7bc39366
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-17 22:32:25 +0530

    47214

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,5 +1,15 @@
 from django.db import models
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+from django.contrib.auth.models import User
+class Order(models.Model):
+    user = models.Foreignkey(User, on_delete=models.CASCADE, related_name="orders")
+    date = models.DataTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
     def __str__(self):
-        return self.name
+        return f"Order {self.id} by {self.user.username}"
+        class OrderItem(models.Model):
+            oder = models.Foreignkey(order, on_delete=models.CASCADE, related_name="items")
+            product_name = models.CharField(max_length=100)
+            quantity = models.PositiveIntegerField()
+            price = models.DecimalField(max_digits=10, decimal_places=2)
+            def __str__(self):
+                return f"{self.product_name} (x{self.quantity})"
