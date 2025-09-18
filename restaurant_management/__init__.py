c5fa4c69f329420aa4b6b97d1e266fc3d70a89c3
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-18 22:32:08 +0530

    47211

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,9 +1,14 @@
-class MenuItemsByCategoryView(APIView):
+from rest_framework.views import APIView
+from rest_frmework.response import Rersponse
+from rest_framework import status
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+class MenuItemsByCategory(APIView):
     def get(self, request):
-        category = request.query_params.get('category', None)
+        category = request_params.get('category')
         if category:
-            items = MenuItem.objects.filter(category__name__iexact=category)
+            items = MenuItem.objects.filter(category=category)
         else:
             items = MenuItem.objects.all()
-        serializer = MenuItemSerializer(items, many=True)
-        return Response(serializer.data)   
+            serializer = MenuItemSerializer(items, many=True)
+            return Response(serializer.data,status=status.HTTP_200_ OK)
