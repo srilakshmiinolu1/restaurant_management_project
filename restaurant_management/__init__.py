15115b96d89809846cfae07563d7dc86a37ce12f
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-18 22:45:21 +0530

    47212

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,14 +1,12 @@
-from rest_framework.views import APIView
-from rest_frmework.response import Rersponse
-from rest_framework import status
-from .models import MenuItem
-from .serializers import MenuItemSerializer
-class MenuItemsByCategory(APIView):
-    def get(self, request):
-        category = request_params.get('category')
-        if category:
-            items = MenuItem.objects.filter(category=category)
-        else:
-            items = MenuItem.objects.all()
-            serializer = MenuItemSerializer(items, many=True)
-            return Response(serializer.data,status=status.HTTP_200_ OK)
+from django.core.validators import
+validate_email
+from django.core.exceptions import 
+ValidationError
+def is_valid_email(email: str) -> bool:
+    try:
+        validation_email(email)
+        return True
+    except ValidationError:
+        return False
+    except Exception:
+        return False
