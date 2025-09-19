2302bc58436db07cba860764ff8fb58f2ae8b9fe
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-19 21:54:33 +0530

    48987

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,16 +1,16 @@
-from djnago.db import models
+from django.db import models
 from django.conf import settings
 from home.models import Product
 class Order(models.Model):
-    order_id = models.Charfield(max_length=100, unique=True)
-    costomer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
-    total_price = models.DecimalField(max_digit=10, decimal_places=2)
+    order_id = models.CharField(max_length=100, unique=True)
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return f"order{self.order_id} by {self.customer}"
+        return f"Order{self.order_id} by {self.customer}"
 class OrderItem(models.Model):
     order = models.ForeignKey(order, on_delete=models.CASCADE, related_name="items")
-    product = models.ForeignKey(product, in_delete=models.CASCADE)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
