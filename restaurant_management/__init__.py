ea95fc80ac82183594a3c39bebb26c60ce914417
Inolu Srilakshmi <srilakshmiinolu1@gmail.com>
2025-09-22 22:37:55 +0530

    48990

--- restaurant_management/__init__.py
+++ restaurant_management/__init__.py
@@ -1,11 +1,8 @@
-from rest_framework import serializers,generics, permissions
-from django.contrib.auth.models import User
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email']
-class UserProfileUpdateView(generics.UpdateAPIView):
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    def get_object(self):
-        return self.request.user        
+from django.db import models
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    opening_days = models.CharFiled(max_length=100, max_length="Enter days like: Mon,Tue,Wed,Thu,Fri,Sat,Sun ")       
+    def __str__(self):
+        return self.name
+         
